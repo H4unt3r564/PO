6582c726c245dec865e11d8639865d3273a32cf5
JakubBandosz <jakubbandosz11@gmail.com>
2025-05-31 01:37:34 +0200

    Update WypozyczalniaSerwis.java

--- WypozyczalniaSerwis.java
+++ WypozyczalniaSerwis.java
@@ -1,8 +1,22 @@
 import java.util.*;
 
 public class WypozyczalniaSerwis {
+    
+    public class WypozyczalniaSerwis {
+
+    //singleton
+    private static WypozyczalniaSerwis instancja;
+        
     private List<Stacja> stacje = new ArrayList<>();
     private List<Wypozyczenie> wypozyczenia = new ArrayList<>();
+        
+    private WypozyczalniaSerwis() {}
+    public static WypozyczalniaSerwis getInstance() {
+        if (instancja == null) {
+            instancja = new WypozyczalniaSerwis();
+        }
+        return instancja;
+    }
 
     public void wypozyczRower(Uzytkownik uzytkownik, Stacja stacja, Rower rower){
         rower.zablokuj(); // nie wiem czy podaawac jaki rower
