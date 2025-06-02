549007d67af253d9449485383feccec4323dae78
Tomasz <chodtom@o2.pl>
2025-06-02 12:34:01 +0200

    dodanie klasy rower

---
+++ Rower.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rower {
+    private int rowerId;
+    private RowerTyp typ;
+    private boolean czyDostepny;
+    private static int counter = 1;
+
+    public Rower(RowerTyp typ, boolean czyDostepny) {
+        this.rowerId = counter++;
+        this.typ = typ;
+        this.czyDostepny = czyDostepny;
+    }
+
+    // Gettery
+    public int getRowerId() {
+        return rowerId;
+    }
+
+    public RowerTyp getTyp() {
+        return typ;
+    }
+
+    public boolean isCzyDostepny() {
+        return czyDostepny;
+    }
+
+    // Settery
+    public void setTyp(RowerTyp typ) {
+        this.typ = typ;
+    }
+
+    public void setCzyDostepny(boolean czyDostepny) {
+        this.czyDostepny = czyDostepny;
+    }
+
+    // Metody
+    public void zablokuj() {
+        this.czyDostepny = false;
+    }
+
+    public void odblokuj() {
+        this.czyDostepny = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Rower #" + rowerId + " | Typ: " + typ + " | Dostępny: " + (czyDostepny ? "Tak" : "Nie");
+    }
+}
