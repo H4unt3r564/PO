7329c773cb6aa99e28053a3842058b317dcf89c5
H4unt3r564 <jasiekpraga@gmail.com>
2025-06-04 09:22:08 +0200

    Update Main.java
    menu i logowanie

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -56,5 +58,75 @@ public class Main {
         for (Wypozyczenie w : user.getHistoria()) {
             System.out.println(w);
         }
+
+        //"nowy" od testow i dodanie do listy uzytkownikow 
+        Uzytkownik nowy = new Uzytkownik(0, "juan", "pablo", "a@b.c", "1234");
+        serwis.dodajUzytkownika(nowy);
+        //skaner do pobierania z kalwiatury maila i hasla, czyZalogowany puste bo while nie dziala bez pustej zmiennej
+        Scanner skaner = new Scanner(System.in);
+        Uzytkownik zalogowany;
+        //do-while z podawaniem maila i hasla i wywolujacy metode zaloguj z wypozyczalniaserwis; jesli jest zle to bedzie null i petla sie powtorzy
+        do{
+            System.out.println("LOGOWANIE");
+            System.out.println("EMAIL: ");
+            String email = skaner.nextLine();
+            System.out.println("HASLO: ");
+            String haslo = skaner.nextLine();
+            zalogowany = serwis.zaloguj(email, haslo);
+        }while(zalogowany == null);
+
+        int opcja;
+        do{
+            System.out.println("MENU");
+            System.out.println("1 - wypisz stacje");
+            System.out.println("2 - wypisz rowery w konkretnej stacji");
+            System.out.println("3 - wypozycz rower");
+            System.out.println("4 - zwroc rower");
+            System.out.println("5 - wypisz swoja historie");
+            System.out.println("6 - skok w przyszlosc (testowanie czasu)");
+            System.out.println("0 - wyjdz");
+
+            opcja = skaner.nextInt();
+            switch(opcja){
+                case 1:
+                    for (Stacja s: serwis.getStacje()){
+                        System.out.println(s);
+                    }
+                    break;
+                case 2:
+                // narazie tak zostawie ale imo najlepiej dac to jako jedna opcja
+                    for (Stacja s: serwis.getStacje()){
+                        System.out.println(s + ": " + s.getRowery());
+                    }
+                    break;
+                case 3:
+                //wypozyczanie roweru. trzeba bedzie zrobic ze wybiera sobie jaki rower z jakiej stacji
+                    zalogowany.wypozyczRower(rower1, stacja3, serwis2);
+                    break;
+                case 4:
+                //zwracanie roweru
+                    zalogowany.zwrocRower(stacja3, serwis2);
+                    break;
+                case 5:
+                //wypisywanie historii
+                    zalogowany.wypisanieHistorii();
+                    break;
+                case 6:
+                // przeskoczenie czasu (testowanie)
+                //pobiera ostatnie nieskonczone wypozyczenie i przesuwa czas o godzine do przodu (czyli ostatnie w liscie)
+                // if sprawdza czy historia jest pusta czy nie (moze sie wykrzaczyc)
+                    if(!zalogowany.getHistoria().isEmpty()){
+                        Wypozyczenie ostatnie = zalogowany.getHistoria().get(zalogowany.getHistoria().size() - 1);
+                        ostatnie.cofnijCzasStart(60);
+                    }
+                    break;
+                case 0:
+                //ustawia zalogowanego na null i wychodzi z menu
+                    zalogowany = null;
+                    break;
+                default:
+                    System.out.println("Nieprawidlowa opcja. Sprobuj ponownie.");
+            }
+        }while(opcja != 0);
     }
 }
