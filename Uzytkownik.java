b7b2f313e18693904afc56e9a2f1a90632b29dc1
H4unt3r564 <jasiekpraga@gmail.com>
2025-06-04 09:22:55 +0200

    Update Uzytkownik.java
    poprawa wypisanieHistorii i walidacja email

--- Uzytkownik.java
+++ Uzytkownik.java
@@ -51,6 +51,13 @@ public class Uzytkownik{
 
     //konstruktor
     public Uzytkownik(int userId, String imie, String nazwisko, String email, String haslo) {
+    // Walidacja email. Nie mozna bez postaci cos@cos.cos (wszystkie litery i ich ilosc dozwolone oprzocz znakow typu spacja, kropka itp)
+    //Czyli e@d.c przejdzie ale np e@d."spacja" juz nie
+    // jak jest zle to wywali blad (ewentualnie do poprawy/zmiany)
+    if (email == null || !email.matches("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z0-9.-]+$")) {
+        throw new IllegalArgumentException("Nieprawidłowy adres e-mail.");
+    }
+
     this.userId = userId;
     this.imie = imie;
     this.nazwisko = nazwisko;
@@ -60,16 +67,18 @@ public class Uzytkownik{
     }
 
     //wypisanie historii
+    //poprawiona metoda wypisywania historii usera
     public void wypisanieHistorii(){
         if (historia.isEmpty()){
             System.out.println("Brak historii wypozyczen.");
         }
         else{
-            for(int i = 0; i < historia.size(); i++ ){
-                System.out.print("Wypozyczenie #" + (i+1) );
+            for (Wypozyczenie h: historia){
+                    System.out.println(h);
             }
         }
     }
+    
     //wypozyczenie 
     public void wypozyczRower(Rower rower, Stacja stacja, WypozyczalniaSerwis serwis){
         serwis.wypozyczRower(this, stacja, rower);
